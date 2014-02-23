6f72259bfc596a252d772e13e1fe3c686604a95f
J. Fencil <fenciljp@dukes.jmu.edu>
2014-02-22 21:43:07 -0500

    Create change_pwd.php

---
+++ change_pwd.php
@@ -0,0 +1,105 @@
+<?PHP
+require_once("membersite_config.php");
+
+if(!$fgmembersite->CheckLogin())
+{
+    $fgmembersite->RedirectToURL("login.php");
+    exit;
+}
+
+if(isset($_POST['submitted']))
+{
+   if($fgmembersite->ChangePassword())
+   {
+        $fgmembersite->RedirectToURL("changed_pwd.html");
+   }
+}
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+      <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+      <title>H.E.A.R.T</title>
+      <link rel="STYLESHEET" type="text/css" href="fg_membersite.css" />
+      <script type='text/javascript' src='gen_validatorv31.js'></script>
+      <link rel="STYLESHEET" type="text/css" href="pwdwidget.css" />
+      <script src="pwdwidget.js" type="text/javascript"></script>       
+<link rel="icon" 
+type="image/png" 
+href="http://openclipart.org/image/800px/svg_to_png/135571/OCAL_Favorites_Icon_Selected_Green.png">
+</head>
+<body>
+<h2>
+<a href='login-home.php'>Home</a>
+</h2>
+<!-- Form Code Start -->
+<div id='fg_membersite'>
+<form id='changepwd' action='<?php echo $fgmembersite->GetSelfScript(); ?>' method='post' accept-charset='UTF-8'>
+<fieldset >
+
+<h2>Change Password</h2>
+
+<input type='hidden' name='submitted' id='submitted' value='1'/>
+
+<div><span class='error'><?php echo $fgmembersite->GetErrorMessage(); ?></span></div>
+
+<div class='container'>
+    <label for='oldpwd' >Old Password:</label><br/>
+    <div class='pwdwidgetdiv' id='oldpwddiv' ></div><br/>
+    <noscript>
+    <input type='password' name='oldpwd' id='oldpwd' maxlength="50" />
+    </noscript>    
+    <span id='changepwd_oldpwd_errorloc' class='error'></span>
+</div>
+
+<div class='container'>
+    <label for='newpwd' >New Password:</label><br/>
+    <div class='pwdwidgetdiv' id='newpwddiv' ></div>
+    <noscript>
+    <input type='password' name='newpwd' id='newpwd' maxlength="50" /><br/>
+    </noscript>
+    <span id='changepwd_newpwd_errorloc' class='error'></span>
+</div>
+
+<br/><br/><br/>
+<div class='container'>
+    <input type='submit' name='Submit' value='Submit' />
+</div>
+
+</fieldset>
+</form>
+<!-- client-side Form Validations:
+Uses the excellent form validation script from JavaScript-coder.com-->
+
+<script type='text/javascript'>
+// <![CDATA[
+    var pwdwidget = new PasswordWidget('oldpwddiv','oldpwd');
+    pwdwidget.enableGenerate = false;
+    pwdwidget.enableShowStrength=false;
+    pwdwidget.enableShowStrengthStr =false;
+    pwdwidget.MakePWDWidget();
+    
+    var pwdwidget = new PasswordWidget('newpwddiv','newpwd');
+    pwdwidget.MakePWDWidget();
+    
+    
+    var frmvalidator  = new Validator("changepwd");
+    frmvalidator.EnableOnPageErrorDisplay();
+    frmvalidator.EnableMsgsTogether();
+
+    frmvalidator.addValidation("oldpwd","req","Please provide your old password");
+    
+    frmvalidator.addValidation("newpwd","req","Please provide your new password");
+
+// ]]>
+</script>
+
+
+</div>
+<!--
+Form Code End (see html-form-guide.com for more info.)
+-->
+
+</body>
+</html>
