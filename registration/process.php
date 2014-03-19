cd8c66c8a11e557c286306158295e845a905fbcf
Kyle Smith <Kyle.Smith@agilex.com>
2014-03-19 18:21:25 -0400

    Adding JS, AJAX, and PHP stuff

---
+++ registration/process.php
@@ -0,0 +1,26 @@
+<?php
+      include_once('db.php');
+
+ if(isset($_POST[username]) && isset($_POST[password]))
+{
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	
+	 $sql = "INSERT INTO users('username','password') VALUES('$username', '$password')";
+	if( mysql_query($sql) )
+	{
+		echo "Inserted Successfully";
+	}
+	else
+	{
+		   echo "Insertion Failed";
+	}
+	
+	mysql_close($conn);
+											   
+}
+else
+{
+	echo "Something went wrong on process.php"
+}
+?>
