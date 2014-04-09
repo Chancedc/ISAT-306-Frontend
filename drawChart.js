1c708fca018a3cbb496f1aab9b2477d1599776f1
Kyle Smith <Kyle.Smith@agilex.com>
2014-04-09 17:30:18 -0400

    Scummy changes

--- drawChart.js
+++ drawChart.js
@@ -1,22 +1,10 @@
 $(document).ready(function(){
-		$("#get_data").click(function() {
-			$( "#start" ).datepicker();
-			$( "#end" ).datepicker();
-			$( '#get_data' ).on( 'click', function() {
-				$.get( 'ajax-example.php', { 
-					device: $( '#device' ).val(),
-					start:  $( '#start'  ).val(),
-					end:    $( '#end'    ).val() } )
-				.done( function( data ) {
-					var lineChartData = { labels: [], datasets: [] }, ds = { fillColor: '#00cc00', strokeColor: '#006600', pointColor: 'rgba(220,220,220,1)', pointStrokeColor: '#006600', data: [] };
-					$( data ).each( function( i, point ) {
-						lineChartData.labels.push( point.timestamp );
-						ds.data.push( point.power );
-
-					});
-					lineChartData.datasets.push( ds );
-					var myLine = new Chart( document.getElementById("canvas" ).getContext("2d") ).Line( lineChartData );
-				});
-			});
-		});
+		alert("got to js");
+		//	$( "#start" ).datepicker();
+		//	$( "#end" ).datepicker();
+		
+			// $( '#get_data' ).on( 'click', function() {
+			// 		$('#text').append("<p>123</p>");
+			// 	});
+			// 		
 }
