4a25290586ab17271cf5d535c454f60f1fc5cd47
J. Fencil <fenciljp@dukes.jmu.edu>
2014-04-09 22:26:39 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,63 @@
-<?php
+<!DOCTYPE html>
+
+
+<head profile="H.E.A.R.T">
+
+<LINK REL=StyleSheet HREF="main.css" TYPE="text/css" MEDIA=screen>
+<title>H.E.A.R.T</title>
+<link rel="icon" 
+type="image/png" 
+href="http://openclipart.org/image/800px/svg_to_png/135571/OCAL_Favorites_Icon_Selected_Green.png">
+<script type="text/javascript">
+
+function validateEmail()
+{
+	var userEmail = document.getElementById('email').value;
+
+	var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+//Explanation:  Look for any character followed by an @ sign with any charcter(s) following. After the characters look for a '.' followed by two to four characters
+	if(!filter.test(userEmail))
+	{
+		alert('Please provide a valid email address');
+		userEmail.focus;
+	}
+	else
+	{
+		alert('valid email');
+	}
+
+}
+
+</script>
+
+
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a1/jquery.mobile-1.0a1.min.css" />
+	<script src="http://code.jquery.com/jquery-1.4.3.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.0a1/jquery.mobile-1.0a1.min.js"></script>
+
+</head>
+<body>
+
+<div data-role="page">
+	<div data-role="header">
+		<h1>H.E.A.R.T Systems</h1>
+	</div><!-- /header -->
+
+	<div data-role="content">
+<div id="banner"> <center><h2> Welcome to H.E.A.R.T Systems! </h2></center>
+<center>
+<img src="https://scontent-a-iad.xx.fbcdn.net/hphotos-frc3/t1/1964783_10152299771486079_243284233_n.jpg">
+</center>
+<center><h4> Please Register or Login Below</h4></center>
+<center>
+</div>
+
+<ul data-role="listview" data-inset="true">
+            <li><a href="registration.html">Register</a></li>
+            <li><a href="login.html">Log In</a></li>
+        
+           
+       <?php
         include_once('db.php');
 echo "made it to the server"; 
 		$username = mysql_real_escape_string( $_POST["username"] );
@@ -24,3 +83,12 @@ echo "made it to the server";
 		 echo "Failed To Login";
    		}		
 ?>
+    
+        </ul>
+<!-- /content -->
+
+
+</div>
+
+</body>
+</html>
