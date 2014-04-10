0e8eb9d898b24d6aa78416244a56c716b915d8bf
J. Fencil <fenciljp@dukes.jmu.edu>
2014-04-09 22:57:12 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,17 @@
 <?php
+
+header( 'Access-Control-Allow-Origin: *' ); 
+
+
         include_once('db.php');
-echo "made it to the server"; 
+
+
+ 
 		$username = mysql_real_escape_string( $_POST["username"] );
 		$password = mysql_real_escape_string( md5($_POST["password"]) );
-		
-
+ 
 		if( empty($username) || empty($password) )
-			echo "Username and Password Mandatory - from PHP";
+			echo "Username and Password Mandatory. Click back and try again.";
 		else
 		{
 		$sql = "SELECT count(*) FROM userinfo WHERE(
@@ -15,14 +20,16 @@ echo "made it to the server";
 				password='$password')";
  
  
-	    $res = mysql_query($sql);
+ 	        $res = mysql_query($sql);
 		$row = mysql_fetch_array($res);
  
 		if( $row[0] > 0 )
+                {
 		 echo "Login Successful";
+                 header("Location: http://isat306.com/testdata.html");
+                } 
 		else
 		 echo "Failed To Login";
    		}		
 ?>
-
 
